0e6274a4f0b50e0ef295dd6d73866571c0c226bc
unknown <micloren@.sogeti.com>
2011-08-31 13:26:25 -0400

    Forgot the Intl.cs

---
+++ Intl/Intl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Intl
+{
+    public class StringTable
+    {
+        public Dictionary<String, String> dictStrings = new Dictionary<string,string>();
+
+        public StringTable(String filename)
+        {
+            TextReader tr = new StreamReader(filename);
+            String stringOfStrings = tr.ReadToEnd();
+            tr.Close();
+
+            String[] strings = stringOfStrings.Trim().Split('\n');
+
+            foreach (var line in strings)
+            {
+                String[] pq = line.Trim().Split(',');
+                dictStrings.Add(pq[0].Trim(), pq[1].Trim());
+            }
+
+        }
+
+    }
+}
